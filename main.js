66ea373e00712258008319e59d3d5dce61c80a76
Alexey <lpac01@mail.ru>
2024-07-02 19:31:18 +0300

    NewNewNew

--- main.js
+++ main.js
@@ -8,3 +8,16 @@ function showSection(sectionId) {
 function faq() {
     alert('Как установить игру?\n1. Выберите свою операционную систему и кликните на соответсвующую кнопку\n2. После попадания на гугл диск, установите все элементы\n3. Распакуйте установленный файл\n4. Запустите\nПриятной игры :)');
 }
+
+    //page 2
+
+    const carouselllvlItems = document.querySelectorAll('.carousellvl-item');
+    let currentIndex = 0;
+    
+    function showNextItem() {
+      carouselllvlItems[currentIndex].classList.remove('active');
+      currentIndex = (currentIndex + 1) % carouselllvlItems.length;
+      carouselllvlItems[currentIndex].classList.add('active');
+    }
+    
+    setInterval(showNextItem, 5000);
